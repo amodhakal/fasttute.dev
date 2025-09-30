2faef021fe71605b4ba7fc59bd2b104c45ddfd89
Amodh Dhakal <amodhakal@gmail.com>
2025-09-30 02:56:52 -0400

    Init nextjs, clerk, and convex

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import ConvexClientProvider from "@/components/ConvexClientProvider";
+import { ClerkLoaded, ClerkProvider } from "@clerk/nextjs";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import type { Metadata } from "next";
+import { Funnel_Display } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+
+const font = Funnel_Display({
+  weight: "variable",
+});
+
+export const metadata: Metadata = {
+  title: "fasttute.dev",
+  description: "Stop scrubbing. Start learning.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${font.className} antialiased`}>
+        <ClerkProvider>
+          <ClerkLoaded>
+            <ConvexClientProvider>
+              <Toaster position="top-center" />
+              {children}
+              <SpeedInsights />
+              <Analytics />
+            </ConvexClientProvider>
+          </ClerkLoaded>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <p className="text-3xl font-bold">fasttute.dev</p>
+      <p className="">Stop scrubbing. Start learning.</p>
+    </div>
+  );
+}
