beae0e828886b474adc80baa669049a3519458f5
Amodh Dhakal <amodhakal@gmail.com>
2025-10-15 01:54:09 -0400

    Added google meta information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="sV48WkDRuxTipH8HmoGm5D48JWPx0E9AV1tg9heklXg"
+        />
+      </head>
       <body
         className={`${spaceGrotesk.className} antialiased bg-black text-gray-300`}
       >
