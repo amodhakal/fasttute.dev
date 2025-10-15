6fa9653332101d3801d72fd35721972620124af8
Amodh Dhakal <amodhakal@gmail.com>
2025-10-15 01:50:20 -0400

    Added verification page

---
+++ src/app/verification/page.tsx
@@ -0,0 +1,3 @@
+export default async function page() {
+    return <div className="">google-site-verification: google2135375085d143ea.html</div>
+}
--- src/fonts.ts
+++ src/fonts.ts
@@ -2,8 +2,10 @@ import { Space_Grotesk, Poller_One } from "next/font/google";
 
 export const spaceGrotesk = Space_Grotesk({
   weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"],
 });
 
 export const pollerOne = Poller_One({
   weight: "400",
+  subsets: ["latin"],
 });
